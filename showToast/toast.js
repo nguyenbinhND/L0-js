ae7c97be94cf561a1a0f622b8a5b01f089394e9e
nguyenbinhND <binhnv7496@gmail.com>
2023-07-05 10:12:31 +0700

    finish shopCart ver 1

--- showToast/toast.js
+++ showToast/toast.js
@@ -1,15 +1,12 @@
 const toasts = {
   success: {
     icon: '<i class="fas fa-check-circle"></i>',
-    msg: "Thêm sản phẩm thành công !",
   },
   error: {
     icon: '<i class="fas fa-exclamation-triangle"></i>',
-    msg: "Sản phẩm đã đạt đến giới hạn!",
   },
   warning: {
     icon: '<i class="fas fa-exclamation-circle"></i>',
-    msg: "Sản phẩm đã đạt đến giới hạn!",
   },
 };
 
@@ -17,11 +14,6 @@ function createToast(status, mss) {
   let toast = document.createElement("div");
   toast.className = `toast ${status}`;
 
-  //   toast.innerHTML = `
-  //     ${toasts[status].icon}
-  //     <span class="msg">${toasts[status].msg}</span>
-  //     <span class="countdown"></span>
-  //     `;
   toast.innerHTML = `
     ${toasts[status].icon}
     <span class="msg">${mss}</span>
